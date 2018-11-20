89908e389594bd81e9fbde81a3b73e4fe1c7d3a0
iuncuim <iuncuim@gmail.com>
2018-11-20 12:46:31 +0300

    LUCI-APP-PINGCONTROL: added app to connection check

---
+++ applications/luci-app-pingcontrol/luasrc/model/cbi/pingcontrol.lua
@@ -0,0 +1,47 @@
+require 'luci.sys'
+m = Map("pingcontrol", "Ping Control", translate("Server availability check"))
+
+section_gen = m:section(NamedSection, "core", "pingcontrol", translate("General settings"))  -- create general section
+
+enabled = section_gen:option(Flag, "enabled", translate("Enabled"), translate("To switch on/off require a reboot"), translate("Enabled"))  -- create enable checkbox
+  enabled.rmempty = false
+
+check_period = section_gen:option(Value, "check_period",  translate("Period of check, sec"))
+  check_period.default = 60
+  check_period.datatype = "and(uinteger, min(20))"
+  check_period.rmempty = false
+  check_period.optional = false
+
+iface = section_gen:option(Value, "iface",  translate("Ping iface name"))
+  iface.default = "internet"
+  iface.datatype = "network"
+  iface.rmempty = false
+  iface.optional = false
+
+testip = section_gen:option(DynamicList, "testip",  translate("IP address of remote servers"))
+  testip.datatype = "ipaddr"
+  testip.cast = "string"
+  testip.rmempty = false
+  testip.optional = false
+
+sw_before_modres = section_gen:option(Value, "sw_before_modres",  translate("Failed attempts before iface up/down"),  translate("0 - not used"))
+  sw_before_modres.default = 0
+  sw_before_modres.datatype = "and(uinteger, min(0), max(100))"
+  sw_before_modres.rmempty = false
+  sw_before_modres.optional = false
+
+sw_before_sysres = section_gen:option(Value, "sw_before_sysres",  translate("Failed attempts before reboot"),  translate("0 - not used"))
+  sw_before_sysres.default = 0
+  sw_before_sysres.datatype = "and(uinteger, min(0), max(100))"
+  sw_before_sysres.rmempty = false
+  sw_before_sysres.optional = false
+
+function m.on_commit(self)
+  -- Modified configurations got committed and the CBI is about to restart associated services
+end
+
+function m.on_init(self)
+  -- The CBI is about to render the Map object
+end
+
+return m
